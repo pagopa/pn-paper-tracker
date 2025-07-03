12584c766dc8b860085579a104d22642f95f63dc
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2025-07-03 11:18:24 +0200

    [PN-15497] - Init repository

---
+++ src/main/java/it/pagopa/pn/papertracker/config/PnPaperTrackerConfigs.java
@@ -0,0 +1,22 @@
+package it.pagopa.pn.papertracker.config;
+
+import it.pagopa.pn.commons.conf.SharedAutoConfiguration;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ConfigurationProperties(prefix = "pn.paper-tracker")
+@Data
+@Import({SharedAutoConfiguration.class})
+public class PnPaperTrackerConfigs {
+
+    private Dao dao;
+
+
+    @Data
+    public static class Dao {
+
+    }
+}
