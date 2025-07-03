44a8b284e0384da11a7a12c3e0a1bafb3695e3ee
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2025-07-03 11:18:24 +0200

    [PN-15497] - Init repository

---
+++ src/main/java/it/pagopa/pn/papertracker/PaperTrackerApplication.java
@@ -0,0 +1,14 @@
+package it.pagopa.pn.papertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperTrackerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperTrackerApplication.class, args);
+    }
+
+}
