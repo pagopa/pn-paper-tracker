e334238e18cc96a05b33541cc147bb690b3adbaf
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-08-18 14:46:45 +0200

    fixes for dev

--- src/main/java/it/pagopa/pn/papertracker/middleware/queue/consumer/PnEventInboundService.java
+++ src/main/java/it/pagopa/pn/papertracker/middleware/queue/consumer/PnEventInboundService.java
@@ -1,22 +1,15 @@
 package it.pagopa.pn.papertracker.middleware.queue.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sngular.apigenerator.asyncapi.business_model.model.event.OcrDataResultPayload;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import it.pagopa.pn.papertracker.config.PnPaperTrackerConfigs;
-import it.pagopa.pn.papertracker.exception.PaperTrackerException;
-import it.pagopa.pn.papertracker.generated.openapi.msclient.paperchannel.model.SingleStatusUpdate;
 import it.pagopa.pn.papertracker.middleware.queue.consumer.internal.ExternalChannelHandler;
 import it.pagopa.pn.papertracker.middleware.queue.consumer.internal.InternalEventHandler;
-import it.pagopa.pn.papertracker.utils.Utility;
+import it.pagopa.pn.papertracker.middleware.queue.model.ExternalChannelOutcomeEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -26,14 +19,12 @@ public class PnEventInboundService {
     private final ExternalChannelHandler externalChannelHandler;
     private final PnPaperTrackerConfigs cfg;
     private final InternalEventHandler internalEventHandler;
-    private final ObjectMapper objectMapper;
 
     @SqsListener(value = "${pn.paper-tracker.topics.external-channel-to-paper-tracker}")
-    public void externalChannelConsumer(@Payload String message, @Headers Map<String, Object> headers) {
+    public void externalChannelConsumer(ExternalChannelOutcomeEvent message) {
         try {
             log.debug("Handle message from pn-external_channel_to_paper_tracker with message {}", message);
-            SingleStatusUpdate singleStatusUpdate = convertToObject(message, SingleStatusUpdate.class);
-            externalChannelHandler.handleExternalChannelMessage(singleStatusUpdate);
+            externalChannelHandler.handleExternalChannelMessage(message.getDetail());
 
         } catch (Exception ex) {
             log.error("Error processing external channel result message: {}", ex.getMessage(), ex);
@@ -49,12 +40,6 @@ public class PnEventInboundService {
         } catch (Exception ex) {
             log.error("Error processing OCR result message: {}", ex.getMessage(), ex);
         }
-    }
-
-    private <T> T convertToObject(String body, Class<T> tClass){
-        T entity = Utility.jsonToObject(this.objectMapper, body, tClass);
-        if (entity == null) throw new PaperTrackerException("MAPPER_ERROR");
-        return entity;
     }
 
 }
