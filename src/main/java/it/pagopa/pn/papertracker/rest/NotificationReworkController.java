d59ec5260739d26e73650cb4d32e34d7731b0e8c
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-10-15 14:36:02 +0200

    Refactored operationId and controller for retrieveSequenceAndStatusCode API

--- src/main/java/it/pagopa/pn/papertracker/rest/NotificationReworkController.java
+++ src/main/java/it/pagopa/pn/papertracker/rest/NotificationReworkController.java
@@ -17,7 +17,7 @@ public class NotificationReworkController implements NotificationReworkApi {
     private final NotificationReworkService notificationReworkService;
 
     @Override
-    public Mono<ResponseEntity<SequenceResponse>> notificationRework(String statusCode, String deliveryFailureCause, final ServerWebExchange exchange) {
+    public Mono<ResponseEntity<SequenceResponse>> retrieveSequenceAndFinalStatus(String statusCode, String deliveryFailureCause, final ServerWebExchange exchange) {
         return notificationReworkService.notificationRework(statusCode, deliveryFailureCause)
                 .map(ResponseEntity::ok);
     }
