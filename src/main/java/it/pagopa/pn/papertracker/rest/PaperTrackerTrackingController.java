15752ed7a9d1a8e5a8e751c1500daa7dec9e3da0
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2025-11-05 15:49:03 +0100

    PN-15647: mvn-dockerv2-codebuild test

--- src/main/java/it/pagopa/pn/papertracker/rest/PaperTrackerTrackingController.java
+++ src/main/java/it/pagopa/pn/papertracker/rest/PaperTrackerTrackingController.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; 
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
