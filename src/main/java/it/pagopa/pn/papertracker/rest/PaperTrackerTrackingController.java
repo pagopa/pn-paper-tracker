5c145ba1b9fcf28198374253c9f883e68763b3ef
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2025-11-05 14:59:10 +0100

    PN-15647: mvn-dockerv2-codebuild test

--- src/main/java/it/pagopa/pn/papertracker/rest/PaperTrackerTrackingController.java
+++ src/main/java/it/pagopa/pn/papertracker/rest/PaperTrackerTrackingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@RestController 
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 public class PaperTrackerTrackingController implements PaperTrackerTrackingApi {
