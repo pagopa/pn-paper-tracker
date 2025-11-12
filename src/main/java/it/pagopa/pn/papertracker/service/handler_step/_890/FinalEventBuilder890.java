47d8acbc2c67730ad7e681e266f1c3e68eb4f9f3
flaminiaScarciofolo <113031535+flaminiaScarciofolo@users.noreply.github.com>
2025-11-12 18:45:04 +0100

    Refactored final event builder for 890 (#157)

--- src/main/java/it/pagopa/pn/papertracker/service/handler_step/_890/FinalEventBuilder890.java
+++ src/main/java/it/pagopa/pn/papertracker/service/handler_step/_890/FinalEventBuilder890.java
@@ -1,20 +1,17 @@
 package it.pagopa.pn.papertracker.service.handler_step._890;
 
-import it.pagopa.pn.papertracker.exception.PnPaperTrackerValidationException;
-import it.pagopa.pn.papertracker.mapper.PaperTrackingsErrorsMapper;
 import it.pagopa.pn.papertracker.middleware.dao.PaperTrackingsDAO;
-import it.pagopa.pn.papertracker.middleware.dao.dynamo.entity.*;
+import it.pagopa.pn.papertracker.middleware.dao.dynamo.entity.Event;
 import it.pagopa.pn.papertracker.middleware.msclient.DataVaultClient;
-import it.pagopa.pn.papertracker.model.EventStatus;
 import it.pagopa.pn.papertracker.model.HandlerContext;
 import it.pagopa.pn.papertracker.service.handler_step.HandlerStep;
 import it.pagopa.pn.papertracker.service.handler_step.generic.GenericFinalEventBuilder;
-import it.pagopa.pn.papertracker.utils.TrackerUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import static it.pagopa.pn.papertracker.model.EventStatusCodeEnum.*;
+import static it.pagopa.pn.papertracker.model.EventStatusCodeEnum.RECAG003C;
+import static it.pagopa.pn.papertracker.model.EventStatusCodeEnum.RECAG012;
 
 @Component
 @Slf4j
@@ -26,41 +23,22 @@ public class FinalEventBuilder890 extends GenericFinalEventBuilder implements Ha
 
     /**
      * Step che elabora l'evento finale (890) in base alla logica di business definita.
-     * Se l'evento finale non è uno stato di giacenza, viene semplicemente aggiunto alla lista degli eventi da inviare.
-     * Se l'evento finale è uno stato di giacenza e la notifica è stata perfezionata, viene aggiunto alla
-     * lista degli eventi da inviare, altrimenti viene lanciato un errore e salvato sulla tabella degli errori.
-     * @param context Contesto contenente le informazioni necessarie per l'elaborazione dell'evento.
+     * Se l'evento che ha innescato il flusso è il RECAG012, non viene eseguita alcuna azione e si passa allo step successivo.
+     * Questo scenario può presentarsi solo nell'handler dedicato alle response dell OCR
+     * Se l'evento che ha innescato il flusso è un evento finale, viene estratto l'evento finale dal contesto,
+     * viene valutato lo status code e viene determinato lo stato dell'evento.
+     * Infine, l'evento viene aggiunto alla lista degli eventi da inviare.
      * @return Mono(Void)
      */
     @Override
     public Mono<Void> execute(HandlerContext context) {
         log.info("FinalEventBuilder890 execute for trackingId: {}", context.getTrackingId());
-
         Event finalEvent = extractFinalEvent(context);
+        if(finalEvent.getStatusCode().equalsIgnoreCase(RECAG012.name())){
+            return Mono.empty();
+        }
         String statusCode = finalEvent.getStatusCode();
-        if (TrackerUtility.isStockStatus890(statusCode))
-            return context.getPaperTrackings().isRefined() ?
-                    addEventToSend(context, finalEvent, EventStatus.PROGRESS.name()) :
-                    buildError(context, finalEvent);
-
         String eventStatus = evaluateStatusCodeAndRetrieveStatus(RECAG003C.name(), statusCode, context.getPaperTrackings()).name();
         return addEventToSend(context, finalEvent, eventStatus);
     }
-
-    private Mono<Void> buildError(HandlerContext context, Event finalEvent) {
-        log.error("Delivery 890 in stock not completed for trackingId: {}", context.getTrackingId());
-        return Mono.error(new PnPaperTrackerValidationException(
-                "Spedizione 890 non perfezionata in giacenza",
-                PaperTrackingsErrorsMapper.buildPaperTrackingsError(
-                        context.getPaperTrackings(),
-                        finalEvent.getStatusCode(),
-                        ErrorCategory.RENDICONTAZIONE_SCARTATA,
-                        ErrorCause.GIACENZA_RECAG012_ERROR,
-                        "Spedizione 890 non perfezionata in giacenza",
-                        FlowThrow.FINAL_EVENT_BUILDING,
-                        ErrorType.ERROR,
-                        finalEvent.getId()
-                )));
-    }
-
 }
