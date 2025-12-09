a1dfad9f71722e2ae5c0b45ed7d1b40ba3eb95bd
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2025-12-09 11:14:04 +0100

    Modified httpStatus error

--- src/main/java/it/pagopa/pn/papertracker/exception/AttachmentsConfigNotFound.java
+++ src/main/java/it/pagopa/pn/papertracker/exception/AttachmentsConfigNotFound.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class AttachmentsConfigNotFound extends PnInternalException {
 
     public AttachmentsConfigNotFound(String errorCode, String message) {
-        super(message, HttpStatus.NOT_FOUND.value(), errorCode);
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR.value(), errorCode);
     }
 }
