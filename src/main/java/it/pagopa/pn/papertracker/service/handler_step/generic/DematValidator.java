a4a01880aba957ba1102e83491ed18bfcb0629a2
DacunzoMa <matteo.dacunzo@emeal.nttdata.com>
2026-02-02 10:08:45 +0100

    PN-18160 Add @Primary to DematValidator

--- src/main/java/it/pagopa/pn/papertracker/service/handler_step/generic/DematValidator.java
+++ src/main/java/it/pagopa/pn/papertracker/service/handler_step/generic/DematValidator.java
@@ -12,6 +12,7 @@ import it.pagopa.pn.papertracker.utils.TrackerUtility;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Primary
 public class DematValidator implements HandlerStep {
 
     private static final Logger log = LoggerFactory.getLogger(DematValidator.class);
