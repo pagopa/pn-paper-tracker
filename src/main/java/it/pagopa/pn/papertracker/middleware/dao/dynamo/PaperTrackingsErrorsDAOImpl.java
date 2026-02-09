bf1adb0eda1c5fab68f1acebd7dfb83e1428aa85
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2026-02-09 17:05:31 +0100

    [PN-18338] - Added logMetric

--- src/main/java/it/pagopa/pn/papertracker/middleware/dao/dynamo/PaperTrackingsErrorsDAOImpl.java
+++ src/main/java/it/pagopa/pn/papertracker/middleware/dao/dynamo/PaperTrackingsErrorsDAOImpl.java
@@ -1,9 +1,11 @@
 package it.pagopa.pn.papertracker.middleware.dao.dynamo;
 
+import it.pagopa.pn.commons.log.dto.metrics.GeneralMetric;
 import it.pagopa.pn.papertracker.config.PnPaperTrackerConfigs;
 import it.pagopa.pn.papertracker.middleware.dao.PaperTrackingsErrorsDAO;
 import it.pagopa.pn.papertracker.middleware.dao.dynamo.entity.PaperTrackingsErrors;
-import lombok.extern.slf4j.Slf4j;
+import it.pagopa.pn.papertracker.utils.MetricUtils;
+import lombok.CustomLog;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,9 +13,10 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
 import java.time.Instant;
+import java.util.List;
 
 @Component
-@Slf4j
+@CustomLog
 public class PaperTrackingsErrorsDAOImpl extends BaseDao<PaperTrackingsErrors> implements PaperTrackingsErrorsDAO {
 
     private final PnPaperTrackerConfigs pnPaperTrackerConfigs;
@@ -28,6 +31,18 @@ public class PaperTrackingsErrorsDAOImpl extends BaseDao<PaperTrackingsErrors> i
     }
 
     public Mono<PaperTrackingsErrors> insertError(PaperTrackingsErrors paperTrackingsErrors) {
+        String logMessage = "Logging metrics : " + MetricUtils.DimensionName.ERROR_CATEGORY.getValue() + " - " + MetricUtils.DimensionName.PRODUCT_TYPE.getValue();
+        GeneralMetric errorCountMetric = MetricUtils.generateGeneralMetric(
+                MetricUtils.MetricName.ERROR_COUNT,
+                1,
+                List.of(
+                        MetricUtils.generateDimension(MetricUtils.DimensionName.ERROR_CATEGORY, paperTrackingsErrors.getErrorCategory().getValue()),
+                        MetricUtils.generateDimension(MetricUtils.DimensionName.PRODUCT_TYPE, paperTrackingsErrors.getProductType())
+                )
+        );
+
+        log.logMetric(List.of(errorCountMetric), logMessage);
+
         paperTrackingsErrors.setTtl(Instant.now().plus(pnPaperTrackerConfigs.getPaperTrackingsErrorsTtlDuration()).getEpochSecond());
         return putItem(paperTrackingsErrors);
     }
