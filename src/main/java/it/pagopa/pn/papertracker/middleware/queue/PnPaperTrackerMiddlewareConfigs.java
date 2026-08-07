fc623f1e92245398980f633b5da0233962dc18d6
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2026-08-07 10:37:22 +0200

    feat(PN-21054): test AI doc UC-04

--- src/main/java/it/pagopa/pn/papertracker/middleware/queue/PnPaperTrackerMiddlewareConfigs.java
+++ src/main/java/it/pagopa/pn/papertracker/middleware/queue/PnPaperTrackerMiddlewareConfigs.java
@@ -5,10 +5,12 @@ import it.pagopa.pn.papertracker.config.PnPaperTrackerConfigs;
 import it.pagopa.pn.papertracker.middleware.queue.model.DeliveryPushEvent;
 import it.pagopa.pn.papertracker.middleware.queue.model.ExternalChannelEvent;
 import it.pagopa.pn.papertracker.middleware.queue.model.OcrEvent;
+import it.pagopa.pn.papertracker.middleware.queue.model.PaperTrackerErrorEvent;
 import it.pagopa.pn.papertracker.middleware.queue.producer.ExternalChannelOutputsMomProducer;
 import it.pagopa.pn.papertracker.middleware.queue.producer.ExternalChannelToPaperChannelDryRunMomProducer;
 import it.pagopa.pn.papertracker.middleware.queue.producer.ExternalChannelToPaperTrackerMomProducer;
 import it.pagopa.pn.papertracker.middleware.queue.producer.OcrMomProducer;
+import it.pagopa.pn.papertracker.middleware.queue.producer.PaperTrackerErrorsMomProducer;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -54,5 +56,13 @@ public class PnPaperTrackerMiddlewareConfigs {
                 this.pnPaperChannelConfigs.getTopics().getExternalChannelToPaperChannelDryRunQueue(),
                 objMapper,
                 ExternalChannelEvent.class);
+    }
+
+    @Bean
+    public PaperTrackerErrorsMomProducer paperTrackerErrorsMomProducer(SqsClient sqsClient, ObjectMapper objMapper) {
+        return new PaperTrackerErrorsMomProducer(sqsClient,
+                this.pnPaperChannelConfigs.getTopics().getPaperTrackerErrorsQueue(),
+                objMapper,
+                PaperTrackerErrorEvent.class);
     }
 }
